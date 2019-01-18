cfccd1cec10a092ba4d75a78d8129eb896facd9d
vanessacanar <44434875+vanessacanar@users.noreply.github.com>
2019-01-17 19:02:39 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,43 @@
+$("#map").click(function(){
+$("#bird").fadeIn();
+});
+
+$("#bird").click(function(){
+$("#rabbit").fadeIn();
+});
+
+$("#map").click(function(){
+$("#hat").slideUp();
+});
+
+$("#rabbit").click(function(){
+$("#hat").slideDown();
+});
+
+$("#hat").click(function(){
+$("#money").fadeIn();
+});
+
+$("#hat").click(function(){
+$("#map").fadeOut();
+});
+
+$("#hat").click(function(){
+$("#bird").fadeOut();
+});
+
+$("#hat").click(function(){
+$("#rabbit").fadeOut();
+});
+
+$("#hat").click(function(){
+$("#hat").fadeOut();
+});
+
+$("#hat").click(function(){
+$("p").fadeOut();
+});
+
+$("#hat").click(function() {
+    $("#button").text("congrats you found the treasure!");
+    });
